482cb22d67c71c59039131d3124edd0d5e4f3cf4
Quang Dao <qvd@andrew.cmu.edu>
2026-09-12 09:38:43 -0700

    Keep Taylor contract axiom checks in classic test mode

--- ArkLibTest/Data/CodingTheory/ReedSolomon/HiddenDerivative/RootFinding/FastTaylor/Contract.lean
+++ ArkLibTest/Data/CodingTheory/ReedSolomon/HiddenDerivative/RootFinding/FastTaylor/Contract.lean
@@ -3,9 +3,7 @@ Copyright (c) 2026 ArkLib Contributors. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Quang Dao
 -/
-module
-
-public import
+import
 ArkLib.Data.CodingTheory.ReedSolomon.HiddenDerivative.RootFinding.FastTaylor.Contract
 
 /-! # Acceptance checks for the computed Taylor chart contract -/
